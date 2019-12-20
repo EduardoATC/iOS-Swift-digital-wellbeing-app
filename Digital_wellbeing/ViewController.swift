b897495b7433ba73e41ce0d55f5a2a3dd56eff30
Eduardo Terrero Cabrera <eduardo.terrero.c@hotmail.com>
2019-12-20 20:29:57 +0100

    Initial Commit

---
+++ Digital_wellbeing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Digital_wellbeing
+//
+//  Created by Eduardo Antonio Terrero Cabrera on 20/12/2019.
+//  Copyright © 2019 Eduardo Antonio Terrero Cabrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
